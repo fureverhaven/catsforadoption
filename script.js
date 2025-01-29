4d91a3cfcf52bf70883bcdb2d53052b2a9e566fe
fureverhaven <sangthyemsung@gmail.com>
2025-01-29 11:05:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>JavaScript Example</title>
+</head>
+<body>
+
+  <h1>Click the Button</h1>
+  <button onclick="displayMessage()">Click Me</button>
+
+  <!-- JavaScript embedded inside HTML -->
+  <script>
+    // JavaScript code
+function toggleMenu() {
+  // Toggle the 'active' class on the nav-links
+  document.getElementById('nav-links').classList.toggle('active');
+}
+
+      </script>
+
+</body>
+</html>
